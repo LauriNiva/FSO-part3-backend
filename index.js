a8dc0b42b388960f173de585ca3d25c31543993d
Lauri Niva <niva.lauri@gmail.com>
2021-03-23 19:19:42 +0200

    p3-pl-backend: step1 done
    Add api call to api/persons and return list of persons in json using express

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const app = express();
+
+let persons = [
+    {
+        "name": "Arto Hellas",
+        "number": "040-123456",
+        "id": 1
+    },
+    {
+        "name": "Ada Lovelace",
+        "number": "39-44-5323523",
+        "id": 2
+    },
+    {
+        "name": "Dan Abramov",
+        "number": "12-43-234345",
+        "id": 3
+    },
+    {
+        "name": "Mary Poppendieck",
+        "number": "345-43562435",
+        "id": 4
+    },
+    {
+        "name": "Test Testersson",
+        "number": "123-234234",
+        "id": 5
+    }
+
+];
+
+app.get("/api/persons", (req, res) => {
+    res.json(persons);
+});
+
+const PORT = 3001;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
