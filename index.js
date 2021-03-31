f25d4bb8c57161fec58ed19147ac7f87159d4a2b
Lauri Niva <niva.lauri@gmail.com>
2021-03-31 08:59:30 +0300

    P3-pl-backend: 3.17 step5
    Add ability to update person with new number

--- index.js
+++ index.js
@@ -37,6 +37,23 @@ app.get("/api/persons/:id", (req, res) => {
 
 });
 
+app.put("/api/persons/:id", (req, res, next) => {
+
+    const body = req.body;
+
+    const person = {
+        name: body.name,
+        number: body.number,
+    };
+
+    Person.findByIdAndUpdate(req.params.id, person, {new: true})
+    .then(updatedPerson => {
+        res.json(updatedPerson);
+    })
+    .catch(err => next(err));
+
+});
+
 app.delete("/api/persons/:id", (req, res, next) => {
     Person.findByIdAndRemove(req.params.id)
     .then(result => {
